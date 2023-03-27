83dae8c8f7393caa4e4235f267555f7e6d0d46ee
morpheus04 <morpheus.lion04@gmail.com>
2023-03-27 15:24:04 +0000

    uuid + app.py

--- backend-flask/db/sql/users/short.sql
+++ backend-flask/db/sql/users/short.sql
@@ -1,9 +1,7 @@
-from lib.db import db
-
-class UsersShort:
-  def run(handle):
-    sql = db.template('users','short')
-    results = db.query_object_json(sql,{
-      'handle': handle
-    })
-    return results
+SELECT
+  users.uuid,
+  users.handle,
+  users.display_name
+FROM public.users
+WHERE 
+  users.handle = %(handle)s
